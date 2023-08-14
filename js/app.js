6008507eb8d2725c7b086393098d86a4b1110002
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-08-14 18:36:36 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,40 @@
+const md = (d) => {
+  const markdownContent = document.querySelector(".markdown-content");
+
+  fetch(d)
+    .then((response) => response.text())
+    .then((markdown) => {
+      const html = marked(markdown);
+      markdownContent.innerHTML = html;
+
+      const summary = document.querySelectorAll("summary");
+
+      summary.forEach((summary) => {
+        const svgHTML =
+          '<div class="svgCnt"><svg class="arrowSvgSummary" width="14" height="18" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M2.00195 2L6.00195 6L2.00195 10" stroke="#5c14e1" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square" /></svg></div>';
+        summary.insertAdjacentHTML("beforeend", svgHTML);
+
+        const detailsElements = document.querySelectorAll("details");
+
+        detailsElements.forEach((detailsElement) => {
+          const arrowSvgSummary =
+            detailsElement.querySelector(".arrowSvgSummary");
+          detailsElement.addEventListener("toggle", (details) => {
+            if (details.target.open) {
+              arrowSvgSummary.style.transform = "rotate(90deg)";
+            } else {
+              arrowSvgSummary.style.transform = "none";
+            }
+          });
+        });
+      });
+    });
+};
+
+// --------------------------------------------------------------------
+// --------------------------- Fonction -------------------------------
+// --------------------------------------------------------------------
+
 const titleFolder = document.querySelectorAll(".titleFolder");
 
 titleFolder.forEach((title) => {
@@ -7,6 +44,8 @@ titleFolder.forEach((title) => {
 
     parentFolder.classList.toggle("active");
 
+    title.classList.toggle("titleActive");
+
     if (subFolder.style.display === "flex") {
       subFolder.style.display = "none";
     } else {
@@ -14,36 +53,27 @@ titleFolder.forEach((title) => {
     }
   });
 });
+md("/data/welcome.md");
+const welcome = document.querySelector("#summary h1");
+console.log(welcome);
+welcome.addEventListener("click", () => {
+  md("/data/welcome.md");
+});
+const subFolders = document.querySelectorAll(".subFolder div");
 
-const markdownContent = document.querySelector(".markdown-content");
-let data = "/data";
-data += "/informatics/osint.md";
+subFolders.forEach((subFolder) => {
+  subFolder.addEventListener("click", () => {
+    // title.classList.toggle("titleActive");
+    console.log(subFolder);
 
-fetch(data)
-  .then((response) => response.text())
-  .then((markdown) => {
-    const html = marked(markdown);
-    markdownContent.innerHTML = html;
-
-    const summary = document.querySelectorAll("summary");
-
-    summary.forEach((summary) => {
-      const svgHTML =
-        '<div class="svgCnt"><svg class="arrowSvgSummary" width="14" height="18" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M2.00195 2L6.00195 6L2.00195 10" stroke="#5c14e1" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square" /></svg></div>';
-      summary.insertAdjacentHTML("beforeend", svgHTML);
-
-      const detailsElements = document.querySelectorAll("details");
-
-      detailsElements.forEach((detailsElement) => {
-        const arrowSvgSummary =
-          detailsElement.querySelector(".arrowSvgSummary");
-        detailsElement.addEventListener("toggle", (details) => {
-          if (details.target.open) {
-            arrowSvgSummary.style.transform = "rotate(90deg)";
-          } else {
-            arrowSvgSummary.style.transform = "none";
-          }
-        });
-      });
-    });
+    const content = subFolder.textContent.toLowerCase();
+    const p1 = subFolder.parentElement;
+    const p2 = p1.parentElement;
+    const title = p2
+      .querySelector(".titleFolder")
+      .textContent.trim()
+      .toLowerCase();
+    let data = `/data/${title}/${content}.md`;
+    md(data);
   });
+});
